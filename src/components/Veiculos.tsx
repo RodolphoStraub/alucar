4261d4d8e2475fc5248724718b68d3a63a8c5064
RodolphoStraub <89853868+RodolphoStraub@users.noreply.github.com>
2023-09-04 14:34:43 -0300

    assentos portas

--- src/components/Veiculos.tsx
+++ src/components/Veiculos.tsx
@@ -43,7 +43,7 @@ function Veiculos({}: Props) {
           <Cardscar
             carname="Uno"
             price="R$200/dia"
-            text="4 assentos"
+            text="5 assentos"
             text2="4 portas"
             text3="A/C"
             img="https://cdn.adtechpanda.com/09b2c40a-c99d-432d-a064-fc3db5a16e3e/"
@@ -51,7 +51,7 @@ function Veiculos({}: Props) {
            <Cardscar
             carname="Golf"
             price="R$500/dia"
-            text="4 assentos"
+            text="5 assentos"
             text2="4 portas"
             text3="A/C"
             img="https://1.bp.blogspot.com/-lnP1GWRN1bI/WyPUMfZkZPI/AAAAAAAAGUw/sbgSwkOxrwgLkpO-sB5ajfrQnV1PrlXhQCLcBGAs/s1600/VW-Golf-Comfortline-2018-basico%2B%25287%2529.jpg"
@@ -59,7 +59,7 @@ function Veiculos({}: Props) {
            <Cardscar
             carname="Captur"
             price="R$600/dia"
-            text="4 assentos"
+            text="5 assentos"
             text2="4 portas"
             text3="A/C"
             img="https://minasfrance.com.br/uploads/products/versions/colors/nova-captur-zen-03.png"
@@ -67,24 +67,24 @@ function Veiculos({}: Props) {
            <Cardscar
             carname="Mustang"
             price="R$700/dia"
-            text="4 assentos"
-            text2="4 portas"
+            text="2 assentos"
+            text2="2 portas"
             text3="A/C"
             img="https://www.pngmart.com/files/15/Ford-Mustang-Convertible-Car-Ecoboost-Transparent-PNG.png"
           />
            <Cardscar
             carname="Minivan"
             price="R$850/dia"
-            text="2 assentos"
-            text2="4 portas"
+            text="8 assentos"
+            text2="3 portas"
             text3="A/C"
             img="https://img.olx.com.br/images/49/490274473739773.jpg"
           />
            <Cardscar
             carname="Uno"
             price="R$999/dia"
-            text="4 assentos"
-            text2="4 portas"
+            text="2 assentos"
+            text2="2 portas"
             text3="A/C"
             img="https://baoxehoi.net/wp-content/uploads/2020/04/si%C3%AAu-xe-M%E1%BB%B9-Corvette.jpg"
           />
